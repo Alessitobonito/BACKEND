71d3667c84fcfff107bd4e1f777ce7bfdc07016e
Alessandro Olivo <alessandroolivovaldivia@gmail.com>
2024-12-05 05:27:23 -0500

    xd

---
+++ 1_inicios_flask.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+# __name__ > propia de python que sirve para indicar si el archivo en el cual nos encontramos es el archivo principal (el que se esta ejecutando por la terminal). si es el archivo principal su valor sera '__main__', caso contrario tendra otro valor la variable
+app = Flask(__name__)
+# Flask solamente puede tener una instancia en todo el proyecto y esa instancia debe de estar en el archivo principal, sino no podra ejecutarse la instancia de la clase
+
+# decorador: reusar el metodo de una clase sin necesidad de editarlo como tal, solo modifica el comportamiento, para este caso, la ruta configurada
+@app.route('/')
+def inicio():
+    return 'Hola mundo desde flask'
+app.run(debug=True)
